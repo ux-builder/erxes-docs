f8e638c2dc2906528bce9b7dec782e5af693d51f
Javkhaa1234 <javhaanoo13@gmail.com>
2023-12-01 00:51:03 +0800

    update

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -28,7 +28,7 @@ export const h2 = function H2(
 
 function InfoIcon0(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg viewBox="0 0 16 16" aria-hidden="true" {...props}>
+    <svg height="4em" viewBox="0 0 16 16" aria-hidden="true" {...props}>
       <circle cx="8" cy="8" r="8" strokeWidth="0"  />
       <path
         fill="none"
@@ -56,7 +56,7 @@ export function Tip({ children }: { children: React.ReactNode }) {
 
 function InfoIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg viewBox="0 0 19 18" aria-hidden="true" {...props}>
+    <svg height="4em" viewBox="0 0 19 18" aria-hidden="true" {...props}>
       <circle cx="7" cy="3" r="0.3" strokeWidth="0" />
       <path fill-rule="evenodd" d="M6.5 0C3.48 0 1 2.19 1 5c0 .92.55 2.25 1 3 1.34 2.25 1.78 2.78 2 4v1h5v-1c.22-1.22.66-1.75 2-4 .45-.75 1-2.08 1-3 0-2.81-2.48-5-5.5-5zm3.64 7.48c-.25.44-.47.8-.67 1.11-.86 1.41-1.25 2.06-1.45 3.23-.02.05-.02.11-.02.17H5c0-.06 0-.13-.02-.17-.2-1.17-.59-1.83-1.45-3.23-.2-.31-.42-.67-.67-1.11C2.44 6.78 2 5.65 2 5c0-2.2 2.02-4 4.5-4 1.22 0 2.36.42 3.22 1.19C10.55 2.94 11 3.94 11 5c0 .66-.44 1.78-.86 2.48zM4 14h5c-.23 1.14-1.3 2-2.5 2s-2.27-.86-2.5-2z"></path>
       <circle cx="7" cy="3" r="0.4" fill="none" />
@@ -77,7 +77,7 @@ export function Note({ children }: { children: React.ReactNode }) {
 
 function InfoIcon2(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg height="1.5em" viewBox="0 0 24 24">
+    <svg height="4em" viewBox="0 0 24 24">
       <path d="M12 4a8 8 0 1 0 0 16 8 8 0 0 0 0-16zM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12z" fill="#2858b8"></path><path d="M12 14a1 1 0 0 1-1-1V7a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1zm-1.5 2.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z" fill="#2858b8"></path>
       <circle cx="8" cy="4" r=".5" fill="none" />
       </svg>
@@ -98,7 +98,7 @@ export function Info({ children }: { children: React.ReactNode }) {
 
 function InfoIcon3(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg height="1.5em" viewBox="0 0 510 512">
+    <svg height="4em" viewBox="0 0 510 512">
       <path d="M479,447.77,268.43,56.64a8,8,0,0,0-14.09,0L43.73,447.77a8,8,0,0,0,7.05,11.79H472A8,8,0,0,0,479,447.77ZM281.38,411.48h-40v-40h40Zm-4-63.92h-32l-6-160h44Z" fill="#eed320"></path>
       <circle cx="8" cy="4" r=".5" fill="none" />
       </svg>
@@ -119,7 +119,7 @@ export function Caution({ children }: { children: React.ReactNode }) {
 
 function InfoIcon4(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg height="1.5em" viewBox="0 0 584 612">
+    <svg height="4em" viewBox="0 0 584 612">
       <path d="M216.02,611.195c5.978,3.178,12.284-3.704,8.624-9.4c-19.866-30.919-38.678-82.947-8.706-149.952 c49.982-111.737,80.396-169.609,80.396-169.609s16.177,67.536,60.029,127.585c42.205,57.793,65.306,130.478,28.064,191.029 c-3.495,5.683,2.668,12.388,8.607,9.349c46.1-23.582,97.806-70.885,103.64-165.017c2.151-28.764-1.075-69.034-17.206-119.851 c-20.741-64.406-46.239-94.459-60.992-107.365c-4.413-3.861-11.276-0.439-10.914,5.413c4.299,69.494-21.845,87.129-36.726,47.386 c-5.943-15.874-9.409-43.33-9.409-76.766c0-55.665-16.15-112.967-51.755-159.531c-9.259-12.109-20.093-23.424-32.523-33.073 c-4.5-3.494-11.023,0.018-10.611,5.7c2.734,37.736,0.257,145.885-94.624,275.089c-86.029,119.851-52.693,211.896-40.864,236.826 C153.666,566.767,185.212,594.814,216.02,611.195z"></path>
       <circle cx="8" cy="4" r=".5" fill="none" />
       </svg>

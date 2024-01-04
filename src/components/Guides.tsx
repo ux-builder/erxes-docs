8867bc119da5ef796b4caba8bad36ace707a7ea3
Javkhaa1234 <javhaanoo13@gmail.com>
2024-01-04 19:01:17 +0800

    update

--- src/components/Guides.tsx
+++ src/components/Guides.tsx
@@ -3,6 +3,28 @@ import { Heading } from '@/components/Heading'
 
 const guides = [
   {
+    href: '/requirement/customer/sales/pos#erkhet',
+    name: 'Erkhet',
+    description: 'Learn how to authenticate your API requests.',
+  },
+  {
+    href: '/pagination',
+    name: 'Inventory',
+    description: 'Understand how to work with paginated responses.',
+  },
+  {
+    href: '/errors',
+    name: 'Errors',
+    description:
+      'Read about the different types of errors returned by the API.',
+  },
+  {
+    href: '/webhooks',
+    name: 'Webhooks',
+    description:
+      'Learn how to programmatically configure webhooks for your app.',
+  },
+  {
     href: '/authentication',
     name: 'Authentication',
     description: 'Learn how to authenticate your API requests.',
@@ -30,7 +52,7 @@ export function Guides() {
   return (
     <div className="my-16 xl:max-w-none">
       <Heading level={2} id="guides">
-        Guides
+        Sales
       </Heading>
       <div className="not-prose mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/5 pt-10 dark:border-white/5 sm:grid-cols-2 xl:grid-cols-4">
         {guides.map((guide) => (

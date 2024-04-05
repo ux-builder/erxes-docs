d43331571e7a9a7082c5b3dd689bda7eeaf3de91
soyombo <e11iot.soko@gmail.com>
2024-04-05 18:11:55 +0800

    Update Tabs.tsx

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -34,31 +34,37 @@ export const Tab = ({ children }: any) => {
   return <>{children}</>
 }
 
-export const Tabs = ({ children }:any) => {
-    const [activeTab, setActiveTab] = React.useState(0);
-  
-    const handleClick = (index:number) => {
-      setActiveTab(index);
-    };
-  
-    return (
-      <div className="border border-gray-300 rounded-lg overflow-hidden">
-        <div className="flex">
-          {children.map((child:any, index:number) => (
-            <button
-              key={`${child.props.label} - ${index}`}
-              className={`py-2 px-4 cursor-pointer outline-none ${
-                index === activeTab ? 'bg-gray-200 text-gray-800' : 'bg-gray-100 text-gray-600'
-              }`}
-              onClick={() => handleClick(index)}
-            >
-              {child.props.label}
-            </button>
-          ))}
-        </div>
-        <div className="p-4">
-          {children[activeTab]}
-        </div>
+export const Tabs = ({ children }: any) => {
+  const [activeTab, setActiveTab] = React.useState(0)
+
+  const handleClick = (index: number) => {
+    setActiveTab(index)
+  }
+
+  let lastIndex = 0
+
+  if (children.length > 0) {
+    lastIndex = children.length - 1
+  }
+
+  return (
+    <div className="overflow-hidden border-b border-gray-300">
+      <div className="flex">
+        {children.map((child: any, index: number) => (
+          <button
+            key={`${child.props.label} - ${index}`}
+            className={`cursor-pointer px-4 py-2 outline-none ${
+              index === activeTab
+                ? 'bg-gray-200 text-gray-800'
+                : 'bg-gray-100 text-gray-600'
+            } ${index === 0 && 'rounded-tl-lg'} ${index === lastIndex && 'rounded-tr-lg'}  ` }
+            onClick={() => handleClick(index)}
+          >
+            {child.props.label}
+          </button>
+        ))}
       </div>
-    );
-  };
+      <div className="p-4">{children[activeTab]}</div>
+    </div>
+  )
+}
